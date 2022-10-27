61d1327a68839f366a5c91115a43eebce1716790
kleber.barilli <kleber.barilli73@gmail.com>
2022-10-26 22:05:49 -0300

    change prettier rules and fix

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,12 +1,12 @@
-import { useRouter } from "next/router";
+import { useRouter } from 'next/router'
 
 export default function Post() {
-    const router = useRouter();
+    const router = useRouter()
 
     return (
         <>
             <h1>Post {router.query.id}</h1>
             <p>{router.asPath}</p>
         </>
-    );
+    )
 }
